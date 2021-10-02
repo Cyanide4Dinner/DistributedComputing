9af0814f2aae3f50cd046141727b962725bf88f0
CyanideForBreakfast <dwivedi.utkarsh01@gmail.com>
2021-10-02 14:49:25 +0530

    setup spark

--- src/spark/test.py
+++ src/spark/test.py
@@ -1,17 +1,9 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder.getOrCreate()
-
-from pyspark.sql import SparkSession
-
-from datetime import datetime, date
-import pandas as pd
-from pyspark.sql import Row
-
-df = spark.createDataFrame([
-    Row(a=1, b=2., c='string1', d=date(2000, 1, 1), e=datetime(2000, 1, 1, 12, 0)),
-    Row(a=2, b=3., c='string2', d=date(2000, 2, 1), e=datetime(2000, 1, 2, 12, 0)),
-    Row(a=4, b=5., c='string3', d=date(2000, 3, 1), e=datetime(2000, 1, 3, 12, 0))
-])
-
-print("Helllo")
+from pyspark import SparkContext
+logFile = "/opt/spark/data/src/spark/log.txt"
+sc = SparkContext("local", "first app")
+logData = sc.textFile(logFile).cache()
+numKs = logData.filter(lambda s: 'INFO' in s).count()
+numTs = logData.filter(lambda s: 'trial' in s).count()
+print("Lines with K: %i, lines with trial: %i" % (numKs, numTs))
+print("Hello")
+open('/opt/spark/data/src/bubu.txt', 'x')
