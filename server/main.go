b14e5749048891604047f8d3c81bd944b8bc1fb6
CyanideForBreakfast <dwivedi.utkarsh01@gmail.com>
2021-10-12 11:50:36 +0530

    setup server

---
+++ server/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	//"html"
+	"log"
+	"net/http"
+	"io/ioutil"
+	"encoding/json"
+	"os/exec"
+	"os"
+	"bufio"
+	"strings"
+	"time"
+)
+
+type query_struct struct {
+		Exec string
+    Query string
+}
+
+type response_struct struct {
+	Time time.Duration
+	Data [][]string
+}
+
+func mr_txt2list() [][]string {
+	f, _ := os.Open("../tmp/output.txt")
+	scanner := bufio.NewScanner(f)
+	var data [][]string
+	for scanner.Scan(){
+		line := scanner.Text()
+		temp := strings.Split(line[strings.Index(line, "\t")+1:len(line)], ",")
+		data = append(data, temp)
+	}
+	//fmt.Println(data)
+	return data
+}
+
+func mapreduce( rw http.ResponseWriter, req *http.Request ){
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+			panic(err)
+	}
+	log.Println(string(body))
+	var q query_struct
+	err = json.Unmarshal(body, &q)
+	if err != nil {
+			panic(err)
+	}
+	var res response_struct
+	log.Println(q.Query)
+	start := time.Now()
+	cmd := exec.Command("/bin/bash", "../src/mapreduce/scripts/run.bash", q.Query)
+	cmd.Output()
+	elapsed := time.Since(start)
+	res.Time = elapsed
+	res.Data = mr_txt2list()
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	json.NewEncoder(rw).Encode(res)
+}
+
+func main() {
+	http.HandleFunc("/mapreduce", mapreduce)
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprintf(w, "Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":5555", nil))
+}
